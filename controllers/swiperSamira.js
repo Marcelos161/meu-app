81d1d39d81ff11ba6d74997172a77b57220aed57
Marcelo Diogo <marcelos.diogos@outlook.com>
2024-09-25 21:10:27 -0300

    varias coisas

--- controllers/swiperSamira.js
+++ controllers/swiperSamira.js
@@ -1,3 +1,5 @@
+const usuarioLogado = localStorage.getItem('usuario');
+
 // Verifica se é o primeiro acesso
 if (!localStorage.getItem('firstAccess_samira')) {
   window.location.href = 'primeiroAcesso.html';
@@ -23,7 +25,7 @@ function loadFotos(imagens) {
       nextEl: '.fotos-next',
       prevEl: '.fotos-prev',
     },
-    resizeObserver: true,
+    spaceBetween: 25, // Ajuste o valor conforme necessário
   });
 
   const fotosWrapper = document.getElementById('fotos-wrapper');
@@ -34,10 +36,86 @@ function loadFotos(imagens) {
     if(fotoUrl.public_id !== "znjcbsrd3f0jnjqtd7my") {
       const slide = document.createElement('div');
       slide.className = 'swiper-slide';
-      slide.innerHTML = `<img src = "${fotoUrl.url}" alt = "${fotoUrl.display_name}" id = "${fotoUrl.public_id}" class="fotos-swiper">`;
+      slide.innerHTML = `
+      <img src="${fotoUrl.url}" alt="${fotoUrl.display_name}" id="${fotoUrl.public_id}" class="fotos-swiper">
+      <div class="comentarios-list" id="comentarios-list-${fotoUrl.public_id}"></div>
+      <textarea placeholder="Adicione um comentário" class="comentario" id="comentario-${fotoUrl.public_id}"></textarea>
+      <button class="enviar-comentario" data-id="${fotoUrl.public_id}">Enviar</button>`;
       fotosWrapper.appendChild(slide);
     }
   });
+
+  buscarComentarios(); 
+}
+
+async function buscarComentarios() {
+  try {
+    const response = await fetch('https://api-marcelo.netlify.app/.netlify/functions/cloudinary/comentarios');
+    const comentarios = await response.json();
+    console.log(comentarios);
+
+    // Mapeia os comentários para cada foto
+    const fotosIds = new Set(comentarios.map(comentario => comentario.ID_foto)); // Conjunto de IDs de fotos
+
+    fotosIds.forEach(fotoId => {
+      const comentariosList = document.getElementById(`comentarios-list-${fotoId}`);
+      if (comentariosList) {
+        // Limpa a lista de comentários antes de adicionar novos
+        comentariosList.innerHTML = '';
+
+        // Filtra os comentários da foto atual
+        const comentariosFiltrados = comentarios.filter(comentario => comentario.ID_foto === fotoId);
+        
+        // Adiciona os comentários filtrados à lista
+        comentariosFiltrados.forEach(comentario => {
+          const comentarioDiv = document.createElement('div');
+          comentarioDiv.classList.add('comentario-item');
+
+          // Inserindo o nome do usuário, a data e o texto do comentário
+          comentarioDiv.innerHTML = `
+            <div class="comentario-header">
+              <span class="comentario-usuario">${comentario.usuario}</span>
+              <span class="comentario-data">${new Date(comentario.created_at).toLocaleDateString()}</span>
+            </div>
+            <div class="comentario-texto">${comentario.comentario}</div>
+          `;
+
+          // Adicionar o comentário na lista de comentários correspondente à foto
+          comentariosList.appendChild(comentarioDiv);
+        });
+
+        comentariosList.scrollTop = comentariosList.scrollHeight;
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao buscar comentários', error);
+  }
+}
+
+
+async function enviarComentario(fotoId, comentario) {
+  try {
+    const response = await fetch('https://api-marcelo.netlify.app/.netlify/functions/cloudinary/comentarios', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ID_foto: fotoId,
+        comentario: comentario,
+        usuario: usuarioLogado,
+      }),
+    });
+
+    if (response.ok) {
+      console.log('deu certo');
+      buscarComentarios();
+    } else {
+      console.error('Erro ao enviar o comentário');
+    }
+  } catch (error) {
+    console.error('Erro ao enviar o comentário', error);
+  }
 }
 
 // upload de imagens
@@ -62,13 +140,6 @@ function uploadImage() {
   
 }
 
-
-// listar imagens
-
-// Função que faz requisição ao endpoint do Netlify
-
-// funcao de listar imagen que ja esta disponivel
-
 function listarImagens() {
   fetch(urlProduct)
     .then(response => response.json())
@@ -82,6 +153,21 @@ function listarImagens() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  listarImagens();  // Chama a função ao carregar a página
+  listarImagens();
+});
+
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('enviar-comentario')) {
+    const fotoId = event.target.dataset.id;
+    const comentarioInput = document.getElementById(`comentario-${fotoId}`);
+    const textoComentario = comentarioInput.value.trim();
+
+    if (textoComentario) {
+      enviarComentario(fotoId, textoComentario);
+      comentarioInput.value = '';  // Limpar o campo de comentário após enviar
+    } else {
+      alert('Por favor, escreva um comentário antes de enviar.');
+    }
+  }
 });
 
